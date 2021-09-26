9f759af75b6595c1b29e888ab55ddb16016438a5
Prajwol Basnet <prajal.basenet0@gmail.com>
2021-09-26 10:41:56 +0545

    changes

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@ export const COLORS = {
   
   primary: "#ffffff", 
   primary2: "#008744",
-  secondary: "#7289da", // light brown
+  secondary: "#8b9dc3", // light brown
   
   background: "#77aaff",
 
